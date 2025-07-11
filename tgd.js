9b9a2a600468efd6cfeb00f8d2fe8ceae8a87c4e
Nooneb <56129009+hishere@users.noreply.github.com>
2025-07-11 10:51:42 +0800

    更新 tgd.js

--- tgd.js
+++ tgd.js
@@ -1,1 +1,17 @@
-console.log(128222)
+var readline = require("readline");
+const {StringSession} = require("telegram/sessions");
+const {TelegramClient, Api} = require("telegram");
+
+const apiId = process.env.TG_ID;
+const apiHash = process.env.TG_HASH;
+const stringSession = new StringSession(process.env.TG_SESSION); // fill this later with the value from session.save()
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
+console.log("apiId:", apiId ? "已设置" : "未设置");
+console.log("apiHash:", apiHash ? "已设置" : "未设置");
+console.log("stringSession:", stringSession.save() ? "已加载" : "未加载");
