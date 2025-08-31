0f0a2e532c33883b201de57c08b7c90a273e24cd
Nooneb <56129009+hishere@users.noreply.github.com>
2025-09-01 06:38:22 +0800

    更新 do_unhide.ps1

--- do_unhide.ps1
+++ do_unhide.ps1
@@ -3,13 +3,14 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 Stop-Process -Name "explorer" -Force
 Write-Host "File extensions are now visible. Explorer will restart automatically."
 
-Start-Sleep -Seconds 3
+$desktop = [Environment]::GetFolderPath('Desktop')
+$edgePath = "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+$shortcutPath = Join-Path $desktop "Edge - 跳过欢迎页.lnk"
 
-#启动msedge并关闭
-Start-Process -FilePath "msedge.exe"
-Start-Process -FilePath "chrome.exe"
-
-Start-Sleep -Seconds 3
-
-Stop-Process -Name "msedge" -Force
-Stop-Process -Name "chrome" -Force
+# 创建 WScript.Shell COM 对象
+$WshShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut($shortcutPath)
+$Shortcut.TargetPath = $edgePath
+$Shortcut.Arguments = "--no-first-run"
+$Shortcut.IconLocation = $edgePath
+$Shortcut.Save()
