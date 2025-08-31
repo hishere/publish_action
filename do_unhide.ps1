0ae1bb13bb03b135e033a3352158f13314dd440d
Nooneb <56129009+hishere@users.noreply.github.com>
2025-09-01 06:27:59 +0800

    更新 do_unhide.ps1

--- do_unhide.ps1
+++ do_unhide.ps1
@@ -3,6 +3,8 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 Stop-Process -Name "explorer" -Force
 Write-Host "File extensions are now visible. Explorer will restart automatically."
 
+Start-Sleep -Seconds 3
+
 #启动msedge并关闭
 Start-Process -FilePath "msedge.exe"
 Start-Process -FilePath "chrome.exe"
