9bf34debc8fdf35d7188d3535f583b4b864b034e
Nooneb <you@example.com>
2026-07-31 12:34:48 +0800

    1

--- sss.ps1
+++ sss.ps1
@@ -28,11 +28,7 @@ Start-Process -FilePath ".\askk4045.exe"
 
 Start-Sleep -Seconds 5
 
-# 执行鼠标操作序列，同意，install
-[MouseSimulator]::ClickAt(620, 530)
-Start-Sleep -Seconds 3
-[MouseSimulator]::ClickAt(677, 567)
-Start-Sleep -Seconds 12
+
 
 #Start-Process -FilePath "C:\Program Files\AskLink\AskLinkLauncher.exe"
 
@@ -59,6 +55,12 @@ public class MouseSimulator {
 }
 "@
 
+
+# 执行鼠标操作序列，同意，install
+[MouseSimulator]::ClickAt(620, 530)
+Start-Sleep -Seconds 3
+[MouseSimulator]::ClickAt(677, 567)
+Start-Sleep -Seconds 12
 
 # 引用必需程序集
 Add-Type -AssemblyName System.Windows.Forms
